9d7dbab97eb82ef1d2797a26c36e9cecf39d99d8
Moussa Kalam AMZAT <m.amzat@alustudent.com>
2023-10-18 12:37:43 +0200

    feat: ES6 string addition

---
+++ Exercise1/joinStrings.js
@@ -0,0 +1,6 @@
+// A function that concatenates strings without using `+`, `.concat()` and `.join()`
+// There must be a space between the two strings
+
+const joinStrings = (string1, string2) => `${string1}  ${string2}`;
+
+console.log(joinStrings("Hello", "World"));
