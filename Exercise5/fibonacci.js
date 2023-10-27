5d14d49fcfb5ebb432d5c2df83531da6dea8fbef
Moussa-Kalam <m.amzat@alustudent.com>
2023-10-27 10:01:05 +0200

    refactor: update variable names

--- Exercise5/fibonacci.js
+++ Exercise5/fibonacci.js
@@ -2,17 +2,17 @@ const calculateFibonacci = (number) => {
   if (number < 2) return number;
 
   // Initialize variables to keep track of Fibonacci elements
-  let element1 = 0,
-    element2 = 1,
+  let fibonacciNumber1 = 0,
+    fibonacciNumber2 = 1,
     currentElement; // Variable to calculate and store the current Fibonacci element
 
   for (let i = 2; i <= number; i++) {
     // Calculate the current Fibonacci element by adding the last two elements
-    currentElement = element1 + element2;
+    currentElement = fibonacciNumber1 + fibonacciNumber2;
 
     // Update the values of element1 and element2 for the next iteration
-    element1 = element2;
-    element2 = currentElement;
+    fibonacciNumber1 = fibonacciNumber2;
+    fibonacciNumber2 = currentElement;
   }
 
   // Final calculated Fibonacci element
